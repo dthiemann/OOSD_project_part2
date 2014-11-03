f8666659b8387d34d2b282a6b7fa9b18b426b9d6
Dylan Thiemann <dylanthiemann@gmail.com>
2014-11-03 15:10:12 -0600

    Added LinearFileDatabase.java which utilizes the Database.java interface

---
+++ OOSD_project_part2/src/org/uiowa/cs2820/engine/LinearFileDatabase.java
@@ -0,0 +1,23 @@
+package org.uiowa.cs2820.engine;
+
+public class LinearFileDatabase implements Database {
+
+	@Override
+	public Node fetch(byte[] key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(byte[] key, String id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void store(byte[] key, String id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
