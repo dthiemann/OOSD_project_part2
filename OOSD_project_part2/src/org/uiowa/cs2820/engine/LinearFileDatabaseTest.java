5b755100f9fa269f34ea1dfe713b7e358ec95d5d
Dylan Thiemann <dylanthiemann@gmail.com>
2014-11-04 15:05:36 -0600

    Added sampleTests in LinearFileDatabseTests.java

--- OOSD_project_part2/src/org/uiowa/cs2820/engine/LinearFileDatabaseTest.java
+++ OOSD_project_part2/src/org/uiowa/cs2820/engine/LinearFileDatabaseTest.java
@@ -1,7 +1,20 @@
 package org.uiowa.cs2820.engine;
 
 import static org.junit.Assert.*;
+import java.util.*;
+import org.junit.Before;
+import org.junit.Test;
 
 public class LinearFileDatabaseTest {
+	
+	@Before
+	public void setUp() {
+		// set up tests, if necessary
+	}
+	
+	@Test
+	public void sampleTest() {
+		assertEquals(0,0);
+	}
 
 }
