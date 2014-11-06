f038397bb4aca4c981be2703d08679fa3b0cb55c
HaxBagoosh <jason@holyarmy.org>
2014-11-06 13:49:04 -0600

    DiskSpace Skeleton
    Just wrote a skeleton for DiskSpace that I will build off of.

---
+++ OOSD_project_part2/src/org/uiowa/cs2820/engine/DiskSpace.java
@@ -0,0 +1,17 @@
+package org.uiowa.cs2820.engine;
+
+public class DiskSpace {
+  
+  DiskSpace () {
+	// constructor does nothing now
+    }
+  
+  public void WriteArea(area#, byte[], "FileName") {	//area# is a place holder for now
+	  
+    }
+  
+  public byte[] ReadArea(area#, "FileName") {	//area# is a place holder for now
+	  
+  	}
+  
+  }
