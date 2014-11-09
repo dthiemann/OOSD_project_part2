7f7f1d05523d1f434b304d96bfc5dd68e3f13a36
HaxBagoosh <jason@holyarmy.org>
2014-11-08 22:09:52 -0600

    Next stage of Disk Space
    I think I'm almost done with this. Have yet to test it though. Where
    exactly will it write this file? Will it be able to find it once it is
    written?

--- OOSD_project_part2/src/org/uiowa/cs2820/engine/DiskSpace.java
+++ OOSD_project_part2/src/org/uiowa/cs2820/engine/DiskSpace.java
@@ -1,17 +1,26 @@
 package org.uiowa.cs2820.engine;
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 public class DiskSpace {
-  
-  DiskSpace () {
-	// constructor does nothing now
+	
+	public void WriteArea(int area, byte[] data, String FileName) throws IOException {
+		RandomAccessFile file = new RandomAccessFile(FileName, "rw");
+		file.seek(area);
+		file.write(data);
+		file.close();
     }
   
-  public void WriteArea(area#, byte[], "FileName") {	//area# is a place holder for now
-	  
-    }
-  
-  public byte[] ReadArea(area#, "FileName") {	//area# is a place holder for now
-	  
+	public byte[] ReadArea(int area, String FileName) throws IOException {
+		RandomAccessFile file = new RandomAccessFile(FileName, "r");
+		file.seek(area);
+		byte[] bytes = new byte[1024]; //need to specify size of byte array somehow?
+		file.read(bytes);
+		file.close();
+		return bytes;
   	}
-  
-  }
+//Where exactly will it write this file? Will it be able to find it once it is written?
+//Maybe add 3rd method to see what file name I am using from keystorage/idstorage 
+//so that i can send to correct file for the parameter "FileName":ID or KEY
+}
