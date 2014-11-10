23dc8f4d2b3449fda3635950eab78bd04c22b857
Dylan Thiemann <dylanthiemann@gmail.com>
2014-11-10 11:58:27 -0600

    Trying to find a way to implement LinearFileDatabase with what we have - I thinthink we will have to amke some changes

--- OOSD_project_part2/src/org/uiowa/cs2820/engine/LinearFileDatabase.java
+++ OOSD_project_part2/src/org/uiowa/cs2820/engine/LinearFileDatabase.java
@@ -1,14 +1,21 @@
 package org.uiowa.cs2820.engine;
 
 public class LinearFileDatabase implements Database {
+	FieldStorage f_storage;
 	
 	public LinearFileDatabase() {
-		
+		this.f_storage = new FieldStorage();
 	}
 
 	@Override
 	public Node fetch(byte[] key) {
 		// TODO Auto-generated method stub
+		//Node myNode = f_storage.get(key);
+		
+		/*
+		 * Something needs to happen here! Not to sure what.
+		 * Need to access the files using a byte array
+		 */
 		
 		return null;
 	}
