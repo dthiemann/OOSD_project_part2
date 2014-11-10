44dfa69e4bb75a81660565b72a4ad5d9a6960042
HaxBagoosh <jason@holyarmy.org>
2014-11-10 15:16:18 -0600

    DiskSpaceTest sort of works

--- OOSD_project_part2/src/org/uiowa/cs2820/engine/DiskSpaceTest.java
+++ OOSD_project_part2/src/org/uiowa/cs2820/engine/DiskSpaceTest.java
@@ -15,9 +15,10 @@ public class DiskSpaceTest {
 		DiskSpace.WriteArea(4,data,"ID");
 		
 		byte [] databack = DiskSpace.ReadArea(4,"ID");
-		String infoback = databack.toString();
+		String infoback = new String(databack);
 		System.out.println(infoback);
-		assertEquals(info,infoback);
+	assertEquals(info,infoback);
+	//This sort of does work. The difference between info and infoback is that infoback has a crap ton of spaces.
 	}
 
 }
