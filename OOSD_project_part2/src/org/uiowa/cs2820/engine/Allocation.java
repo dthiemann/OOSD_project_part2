956f815a2c78661c2e69d7cc41fb9c942bf933b0
Tim Miller <tim-miller@uiowa.edu>
2014-11-12 17:36:32 -0600

    Allocation/Checkpoint 2nd Drafts

--- OOSD_project_part2/src/org/uiowa/cs2820/engine/Allocation.java
+++ OOSD_project_part2/src/org/uiowa/cs2820/engine/Allocation.java
@@ -1,31 +1,63 @@
+/*
+
+------------------------------------------
+        Allocation Class (Tim M)
+------------------------------------------
+Team 1 (Dylan T) (Jason S) (Joe M) (Tim M)  
+
+*/
+
 package org.uiowa.cs2820.engine;
 
 import java.util.BitSet;
+import java.lang.Object;
+import java.io.*; 
+
+import org.uiowa.cs2820.engine.Checkpoint;
 
 public class Allocation {
 
-	static String bs_name;
+	private static BitSet bs;
 
-	Allocation() {
-		BitSet bs = new BitSet();
-		bs_name = bs.toString();
-		Checkpoint.save(bs);
-	}
 
 	/**
 	 * Returns a single free area in memory
 	 */
 	public static int allocate() {
-		BitSet bs = (BitSet) Checkpoint.restore(bs_name);
-		int m = bs.nextClearBit(0);
-		bs.set(m);
 		Checkpoint.save(bs);
-		return m;
+		int freespace;
+		if (bs.isEmpty()){
+			freespace = 0;
+		}
+		else{
+			freespace = bs.nextClearBit(0);
+		}
+		bs.set(freespace);
+		Checkpoint.save(bs);
+		return freespace;
 	}
 
 	public static void free(int areaNum) {
-		BitSet bs = (BitSet) Checkpoint.restore(bs_name);
 		bs.clear(areaNum);
+		Checkpoint.save(bs);
+		
+	}
+
+	public int size(){
+		return bs.cardinality();
+		
+	}
+
+public static void main(String [ ] args){
+		try {
+			File file = new File("a.txt");
+			bs = (BitSet) Checkpoint.restore(file);
+			
+			FileOutputStream f = new FileOutputStream(file);
+			
+			if (!file.exists()){
+				file.createNewFile();
+			}
 	}
 }
-
+}
