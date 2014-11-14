83e6ad4988bca299b70b2ee6a2341120c487cc41
HaxBagoosh <jason@holyarmy.org>
2014-11-14 17:33:24 -0600

    IntegrationTest is FieldTest
    Prof Herman's FieldTest is already an adequate integration test.

---
+++ OOSD_project_part2/src/org/uiowa/cs2820/engine/IntegrationTest.java
@@ -0,0 +1,12 @@
+package org.uiowa.cs2820.engine;
+//I (Jason Sherman) was going to write this but FieldTest already does an adequate Integration tests and runs the tests
+//that i was planning to run.
+public class IntegrationTest {
+//User uses: Indexer, Field, FieldSearch
+	//Create a field with a key value pair, with an identifier, send to indexer (add to database).
+	//Add some other random field,identifier to database.
+	//Add another field that is identical to first field with a different identifier.
+	//(We are searching for fields, returning identifiers)
+	
+	
+}
