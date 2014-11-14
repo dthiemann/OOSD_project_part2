5d82023da659115177afa13bfedc7043b41da3d8
jmaule <joseph-maule@uiowa.edu>
2014-11-13 23:35:19 -0600

    For testing purposes

---
+++ OOSD_project_part2/src/org/uiowa/cs2820/engine/MockAllocation.java
@@ -0,0 +1,57 @@
+package org.uiowa.cs2820.engine;
+
+public class MockAllocation {
+	private static int[] bitField = {0};
+	private static int[] bitIDs = {0};
+	
+	public static int allocate(String filename){
+		int[] bits = getFile(filename);
+		for(int i = 1; i < bits.length; i++){
+			if( bits[i] == 0){
+				bits[i] = 1;
+				return i;
+			}
+		}
+		// If all bits set, expand array
+		expandBits(bits);
+		return allocate(filename);
+	}
+	
+	public static int fs(String filename){
+		int[] bits = getFile(filename);
+		for(int i = 1; i < bits.length; i++){
+			if( bits[i] == 0){
+				return i;
+			}
+		}
+		// If all bits set, expand array
+		expandBits(bits);
+		return allocate(filename);
+	}
+	
+	public static void free(String filename, int index){
+		int[] bits = getFile(filename);
+		bits[index] = 0;
+	}
+	
+	public static int size(String filename){
+		int[] bits = getFile(filename);
+		return bits.length;
+	}
+	
+	private static int[] getFile(String filename){
+		if( filename == "FIELD"){
+			return bitField;
+		}
+		else{
+			return bitIDs;
+		}
+	}
+	
+	private static void expandBits(int[] bits){
+		int[] newBits = new int[bits.length * 2];
+		System.arraycopy(bits, 0, newBits, 0, bits.length);
+		bits = newBits;
+	}
+	
+}
