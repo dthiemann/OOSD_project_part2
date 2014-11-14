2801ae90dae1eae02cd23e4631f7faa4b6c27238
Tim Miller <tim-miller@uiowa.edu>
2014-11-14 15:09:29 -0600

    Allocation/Checkpoint 4th Submission
    Latest commit of Allocation/Checkpoint classes

--- OOSD_project_part2/src/org/uiowa/cs2820/engine/Allocation.java
+++ OOSD_project_part2/src/org/uiowa/cs2820/engine/Allocation.java
@@ -9,61 +9,64 @@ Team 1 (Dylan T) (Jason S) (Joe M) (Tim M)
 
 package org.uiowa.cs2820.engine;
 
-import java.io.File;
 import java.util.BitSet;
 
 public class Allocation {
 
 	private static BitSet bs;
 
-
-	/**
-	 * Returns a single free area in memory
-	 */
-	public static int allocate() {
-		Checkpoint.save(bs);
+	/*
+	 Returns a single free area in memory
+	*/
+	public static int allocate(String param) throws Exception {
+		bs = Checkpoint.restore(param);
+		Checkpoint.save(bs, param);
 		int freespace;
 		if (bs.isEmpty()){
 			freespace = 0;
 		}
 		else{
-			freespace = bs.nextClearBit(0);
+			freespace = bs.nextClearBit(1);
 		}
 		bs.set(freespace);
-		Checkpoint.save(bs);
+		Checkpoint.save(bs, param);
 		return freespace;
 	}
 	
-	public static int fs(){
+	/*
+	 returns index of free slot 
+	 in ID BitArray w/o calling allocate()
+	*/
+	public static int fs() throws Exception{
+		bs = Checkpoint.restore("ID");
 		if (bs.isEmpty()){
 			return 0;
 		}
 		else{
-		    return bs.nextClearBit(0);
+		    return bs.nextClearBit(1);
 		}
 	}
 
-	public static void free(int areaNum) {
+	//frees up areaNum
+	public static void free(int areaNum, String param) throws Exception {
+		bs = Checkpoint.restore(param);
 		bs.clear(areaNum);
-		Checkpoint.save(bs);
-		
+		Checkpoint.save(bs, param);
 	}
 
-	public int size(){
-		return bs.cardinality();
-		
-	}
-
+	public int size() throws Exception{
+		bs = Checkpoint.restore("FIELD");
+		return bs.length();
+	}		
+}
+/*
 public static void main(String [ ] args){
 		try {
 			File file = new File("a.txt");
 			bs = (BitSet) Checkpoint.restore("a.txt");
 			
-			//FileOutputStream f = new FileOutputStream(file);
-			
 			if (!file.exists()){
 				file.createNewFile();
 			}
 	}       catch(Exception e){}
-}
-}
+} */
--- OOSD_project_part2/src/org/uiowa/cs2820/engine/Checkpoint.java
+++ OOSD_project_part2/src/org/uiowa/cs2820/engine/Checkpoint.java
@@ -9,47 +9,77 @@ Team 1 (Dylan T) (Jason S) (Joe M) (Tim M)
 
 package org.uiowa.cs2820.engine;
 
-import org.uiowa.cs2820.engine.Field;
 import java.io.*;
+import java.util.BitSet;
 
 public class Checkpoint {
-	static File file;
+	
+	private final static File file1 = new File("a.txt");
+	private final static File file2 = new File("b.txt");
 
 	// (in 'a.txt') saves object as byte array
-	public static void save(Object O) {
-		byte[] data = new byte[(int) file.length()];
-		data = Field.convert(O);
+	public static void save(Object O, String param) throws Exception {
+		File temp = getFile(param);
+		checkFile(temp);
+		byte[] data = new byte[(int) temp.length()];
+		data = Utility.convert(O);
 		try {
-			FileOutputStream out = new FileOutputStream(file);
+			FileOutputStream out = new FileOutputStream(temp);
 			out.write(data);
 			out.close();
 		} catch (Exception e) {
-
 		}
 	}
 
 	// (in 'a.txt') saves byte array as object
-	public static Object restore(String file) throws Exception {
-		FileInputStream fs = new FileInputStream(file);
-		byte[] b = new byte[(int) file.length()];
+	public static BitSet restore(String file) throws Exception {
+		File temp = getFile(file);
+		checkFile(temp);		
+		FileInputStream fs = new FileInputStream(temp);
+		byte[] b = new byte[(int) temp.length()];
 		fs.read(b);
-		Object O = new Object();
-		O = Field.revert(b);
+		BitSet O = (BitSet) Utility.revert(b);
 		fs.close();
 		return O;
 	}
 
+	// returns which file to save/restore
+	private static File getFile(String param){ 
+ 		if( param == "FIELD"){ 
+ 			return file1; 
+ 		} 
+ 		else{ 
+ 			return file2;  		} 
+}
+	
+	//
+	private static void checkFile(File f) throws Exception{
+		if (!f.exists()){
+			f.createNewFile();
+			FileInputStream fs = new FileInputStream(f);
+			byte[] b = new byte[(int) f.length()];
+			fs.read(b);
+			BitSet O = new BitSet();
+			O = (BitSet) Utility.revert(b);
+			O.clear(0);
+			byte[] b1 = new byte[(int) O.length()];
+			b1 = Utility.convert(O);
+			FileOutputStream out = new FileOutputStream(f);
+			out.write(b1);
+			out.close();
+			fs.close();
+			}
+	}
+}	
+	/*
 	public static void main(String[] args) {
 		try {
 			file = new File("a.txt");
-			//BufferedReader br = new BufferedReader(new FileReader(file));
-
+			
 			if (!file.exists()) {
 				file.createNewFile();
 			}
 		} catch (Exception e) {
 
 		}
-	}
-
-}
+	}*/
