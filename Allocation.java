d2eeef984d799b9ca66f054fdc6b9bcbcbd86667
Tim Miller <tim-miller@uiowa.edu>
2014-11-13 20:39:30 -0600

    Allocation/Checkpoint 3rd Drafts
    (Hopefully) these are almost final.

--- Allocation.java
+++ Allocation.java
@@ -1,31 +1,69 @@
+/*
+
+------------------------------------------
+        Allocation Class (Tim M)
+------------------------------------------
+Team 1 (Dylan T) (Jason S) (Joe M) (Tim M)  
+
+*/
+
 package org.uiowa.cs2820.engine;
 
+import java.io.File;
 import java.util.BitSet;
 
 public class Allocation {
 
-	static String bs_name;
+	private static BitSet bs;
 
-	Allocation() {
-		BitSet bs = new BitSet();
-		bs_name = bs.toString();
-		Checkpoint.save(bs);
-	}
 
 	/**
 	 * Returns a single free area in memory
 	 */
 	public static int allocate() {
-		BitSet bs = (BitSet) Checkpoint.restore(bs_name);
-		int m = bs.nextClearBit(0);
-		bs.set(m);
 		Checkpoint.save(bs);
-		return m;
+		int freespace;
+		if (bs.isEmpty()){
+			freespace = 0;
+		}
+		else{
+			freespace = bs.nextClearBit(0);
+		}
+		bs.set(freespace);
+		Checkpoint.save(bs);
+		return freespace;
+	}
+	
+	public static int fs(){
+		if (bs.isEmpty()){
+			return 0;
+		}
+		else{
+		    return bs.nextClearBit(0);
+		}
 	}
 
 	public static void free(int areaNum) {
-		BitSet bs = (BitSet) Checkpoint.restore(bs_name);
 		bs.clear(areaNum);
+		Checkpoint.save(bs);
+		
 	}
-}
 
+	public int size(){
+		return bs.cardinality();
+		
+	}
+
+public static void main(String [ ] args){
+		try {
+			File file = new File("a.txt");
+			bs = (BitSet) Checkpoint.restore("a.txt");
+			
+			//FileOutputStream f = new FileOutputStream(file);
+			
+			if (!file.exists()){
+				file.createNewFile();
+			}
+	}       catch(Exception e){}
+}
+}
--- Checkpoint.java
+++ Checkpoint.java
@@ -1,24 +1,55 @@
+/*
+
+------------------------------------------
+         Checkpoint Class (Tim M)
+------------------------------------------
+Team 1 (Dylan T) (Jason S) (Joe M) (Tim M)  
+
+ */
+
 package org.uiowa.cs2820.engine;
 
+import org.uiowa.cs2820.engine.Field;
+import java.io.*;
 
 public class Checkpoint {
-	
-	static Database db;
-	static String name;
-	
-	Checkpoint(Object O){
-		name = O.toString();
-		db = new LinearMemoryDatabase();
-	}
+	static File file;
 
+	// (in 'a.txt') saves object as byte array
 	public static void save(Object O) {
-		byte[] data = Field.convert(O);
-		db.store(data, name);
+		byte[] data = new byte[(int) file.length()];
+		data = Field.convert(O);
+		try {
+			FileOutputStream out = new FileOutputStream(file);
+			out.write(data);
+			out.close();
+		} catch (Exception e) {
+
+		}
 	}
 
-	public static Object restore(String bs_name) {
-		return null;
+	// (in 'a.txt') saves byte array as object
+	public static Object restore(String file) throws Exception {
+		FileInputStream fs = new FileInputStream(file);
+		byte[] b = new byte[(int) file.length()];
+		fs.read(b);
+		Object O = new Object();
+		O = Field.revert(b);
+		fs.close();
+		return O;
+	}
+
+	public static void main(String[] args) {
+		try {
+			file = new File("a.txt");
+			//BufferedReader br = new BufferedReader(new FileReader(file));
+
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+		} catch (Exception e) {
+
+		}
 	}
 
 }
-
